a39ef647cfee00a67732d584748c8c8d77f7e9ec
Kirill SIMAGIN <kirill.simagin+ora@orange.com>
2026-05-06 17:08:15 +0200

    feat(xcodeproj): add initial Xcode project configuration for Kizba app to set up the project structure and build settings
    chore(xcode): add PBXTargetDependency and XCBuildConfiguration settings for Kizba project to improve build configurations and dependencies management
    
    chore(xcode): add build configurations for Kizba project and its targets to streamline build process and improve organization
    
    feat: initialize Kizba project with basic structure and assets
    
    - Add project workspace configuration for Xcode.
    - Create asset catalog with AccentColor and AppIcon.
    - Implement basic SwiftUI app structure with ContentView and KizbaApp.
    - Include initial test file for Kizba functionality.

---
+++ Kizba/Kizba/KizbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KizbaApp.swift
+//  Kizba
+//
+//  Created by KIRILL SIMAGIN on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct KizbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
